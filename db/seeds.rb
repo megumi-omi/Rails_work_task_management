29aba737dd94ac42fee86d0adf4b5eab4dacb801
megumi <ooo.megumi9@gmail.com>
2023-11-07 08:31:51 +0900

    最初のユーザをseedで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password_digest: password,
+               )
