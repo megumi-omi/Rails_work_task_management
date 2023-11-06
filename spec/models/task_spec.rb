63b7ba1f14ea0e74eee4ec1d62e427d012a8d056
megumi <ooo.megumi9@gmail.com>
2023-11-06 09:40:52 +0900

    titleカラムに検索インデックスを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,16 +9,32 @@ RSpec.describe 'タスクモデル機能', type: :model do
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
         task = Task.new(title: '失敗テスト', content: '')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
         task = Task.new(title: '成功テスト', content: '成功テスト')
         expect(task).to be_valid
+      end
+    end
+  end
+  describe 'scopeのテスト' do
+    context 'タイトルをあいまい検索した場合' do
+      it '該当のタイトルを含むタスクが表示される' do
+        task = Task.create(title: 'title1', content: 'content1', deadline: '2023/12/1', status: 'completed')
+        task = Task.create(title: 'title2', content: 'content2', deadline: '2023/12/10', status: 'working')
+        task = Task.create(title: 'title3', content: 'content3', deadline: '2023/12/20', status: 'waiting')
+        expect(Task.search_title('3')).to include("title3")
+      end
+    end
+    context 'ステータスで検索した場合' do
+      it '該当のステータスを含むタスクが表示される' do
+      end
+    end
+    context 'タイトルをあいまい検索、ステータスを検索した場合' do
+      it '該当のタイトル・ステータスを含むタスクが表示される' do
       end
     end
   end
