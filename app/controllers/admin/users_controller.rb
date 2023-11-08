4c109a3590d28aab06d9ac770242dbf930787954
megumi <ooo.megumi9@gmail.com>
2023-11-08 18:13:53 +0900

    検証のため

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,14 @@
 class Admin::UsersController < ApplicationController
    before_action :user_check
 
+  def user_check
+    if current_user.admin
+    else 
+      redirect_to tasks_path
+      flash[:danger] = "管理者以外アクセスできません"
+    end
+  end
+
   def index
     @users = User.all.includes(:tasks)
   end
@@ -27,10 +35,16 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update!(user_params)
+    #名前だけ変更
+    @user.update_attribute(:name, params[:user][:name])
+    #emailだけ変更
+    @user.update_attribute(:email, params[:user][:email])
+    @user.update_attribute(:admin, params[:user][:admin])
+    if @user.save
       flash[:notice] = "ユーザ情報を更新しました"
       redirect_to admin_users_path
     else
+      binding.irb
       render :edit
     end
   end
@@ -45,18 +59,10 @@ class Admin::UsersController < ApplicationController
     flash[:notice] = "ユーザを削除しました"
     redirect_to admin_users_path
   end
- #名前変える
-  def user_check
-    if current_user.admin
-    else 
-      redirect_to tasks_path
-      flash[:danger] = "管理者以外アクセスできません"
-    end
-  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
